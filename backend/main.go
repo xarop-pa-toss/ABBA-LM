87f875666d700edd6aa39b1e03b69a7ff214554e
xarop-pa-toss <rpgdsantos@gmail.com>
2024-10-07 14:01:08 +0100

    Setup Viper

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	// Viper configuration loading
+	viper.SetConfigFile("./config/.env")
+	viper.ReadInConfig()
+
+	fmt.Println((viper.Get("PORT")))
+
 	fs := http.FileServer(http.Dir("../frontend/dist"))
 	http.Handle("/", fs)
 
