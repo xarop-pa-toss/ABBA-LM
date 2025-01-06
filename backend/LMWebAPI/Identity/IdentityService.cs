92f1cb1d9ca9582b37903df3c3537db194b1c52b
xarop-pa-toss <rpgdsantos@gmail.com>
2025-01-06 02:50:08 +0000

    Implement LoginDTO.cs and credentials verification. Untested

--- backend/LMWebAPI/Identity/IdentityService.cs
+++ backend/LMWebAPI/Identity/IdentityService.cs
@@ -1,18 +1,30 @@
 using Microsoft.AspNetCore.Identity.Data;
 using LMWebAPI.Repositories;
+using LMWebAPI.Resources.Errors;
 using MongoDB.Driver;
+using BCrypt.Net;
+using LMWebAPI.Models.DTOs;
 
 namespace LMWebAPI.Identity;
 
 public class IdentityService<T>(IMongoDatabase database) : MongoRepository<T>(database, "users")
 {
-    public IdentityService<T> ValidateUserCredentials(LoginRequest loginRequest)
+    public async Task<IdentityService<T>> ValidateUserCredentials(LoginRequest loginRequest)
     {
-        var userDetails = new
+        var filter = Builders<T>.Filter.Eq("email", loginRequest.Email);
+        var projection = Builders<T>.Projection.Include("email");
+        
+        var user = await Collection.Find(filter).Project(projection).FirstOrDefaultAsync();
+        if (user is not null)
         {
-            ID = "",
-            Email = loginRequest.Email,
-            Password = loginRequest.Password,
-        };
+            var hashedPw = BCrypt.Net.BCrypt.HashPassword(loginRequest.Password);
+            var storedPw = user.GetValue("password").ToString();
+            
+            if (!BCrypt.Net.BCrypt.Verify(hashedPw, storedPw))
+            {
+                throw new ProblemNotFoundException("Invalid username or password.");
+            }
+        }
+        // MISSING RETURN
     }
 }
