e3ecad00e068ba942f2bf40f4cd090288ae385fc
xarop-pa-toss <rpgdsantos@gmail.com>
2025-05-25 14:29:43 +0100

    feat(BloodTourney): Finish SingleEliminationStrategy.cs first round creation

--- api/BloodTourney/Tournament/Formats/ITournamentFormat.cs
+++ api/BloodTourney/Tournament/Formats/ITournamentFormat.cs
@@ -1,20 +1,20 @@
 ﻿namespace BloodTourney.Tournament;
 
-public interface ITournamentFormat
+internal interface ITournamentFormat
 {
     /// <summary>
     /// Pseudo-randomizes a given list of teams.
     /// </summary>
     /// <param name="playerIds">Collection of all players in the tournament</param>
     /// <returns>Ordered pairs of players for first round matches</returns>
-    IEnumerable<(Guid playerA, Guid? playerB)> CreateFirstRoundRandom(IEnumerable<MatchNode> matchNodes);
+    IEnumerable<MatchNode> CreateFirstRoundRandom(IEnumerable<Guid> teamGuids);
     
     /// <summary>
     /// Creates the initial rounds of a tournament based on the given players NAF Score
     /// </summary>
     /// <param name="playerIdsWithNafScore">Dictionary of player IDs and their rankings</param>
     /// <returns>Ordered pairs of players for first round matches</returns>
-    IEnumerable<(Guid playerA, Guid playerB)> CreateFirstRoundSeeded(Dictionary<Guid, uint> playerIdsWithNafScore);
+    IEnumerable<MatchNode> CreateFirstRoundSeeded(Dictionary<Guid, uint> teamGuidsWithRank);
     
     /// <summary>
     /// Calculate teams' advancement through the rounds.
@@ -22,17 +22,16 @@ public interface ITournamentFormat
     /// <param name="playerStandings">Current tournament standings (points, wins, etc.)</param>
     /// <param name="completedMatches">History of completed matches to avoid rematches</param>
     /// <returns>List of player pairs for the next round</returns>
-    IEnumerable<Guid> CreateNextRound(
-        Dictionary<Guid, (uint wins, uint losses, uint touchdowns)> playerStandings,
-        IEnumerable<(Guid playerA, Guid playerB)> completedMatches);
+    IEnumerable<MatchNode> CreateNextRound(IEnumerable<MatchNode> completedMatches);
 }
 
-public class MatchNode
+internal class MatchNode
 {
-    public Guid PlayerA { get; set; }
-    public Guid? PlayerB { get; set; }
-    public Guid? Winner { get; set; }
-    public Guid? Loser { get; set; }
+    public bool IsBye { get; set; } = false;
+    public Guid? TeamA { get; set; }
+    public Guid? TeamB { get; set; }
+    public Guid? Winner { get; set; } = null;
+    public Guid? Loser { get; set; } = null;
 }
 
 public enum MatchResult
--- api/BloodTourney/Tournament/Formats/SingleEliminationStrategy.cs
+++ api/BloodTourney/Tournament/Formats/SingleEliminationStrategy.cs
@@ -1,27 +1,93 @@
-using System;
-using MongoDB.Bson;
-namespace BloodTourney.Tournament;
+namespace BloodTourney.Tournament.Formats;
 
-public class SingleEliminationStrategy : ITournamentFormat
+internal class SingleEliminationStrategy : ITournamentFormat
 {
-    public IEnumerable<MatchNode> CreateFirstRoundRandom(IEnumerable<Guid> teamGuid)
+    /// <summary>
+    /// First round will have 'byes' if total number of teams is not a power of two.
+    /// 'Byes' are represented by MatchNode objects that have TeamA set but TeamB as null.
+    /// </summary>
+    /// <param name="teamGuids"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    IEnumerable<MatchNode> ITournamentFormat.CreateFirstRoundRandom(IEnumerable<Guid> teamGuids)
     {
-        if (!teamGuid.Any())
+        List<Guid> teamGuidList = teamGuids.ToList();
+        if (!teamGuidList.Any())
         {
-            throw new ArgumentOutOfRangeException("List of teams is empty.");
+            throw new ArgumentOutOfRangeException(nameof(teamGuids));
         }
         
-        int totalTeams = teamGuid.Count();
-        int nextPowerOfTwo = Helpers.GetNextPowerOfTwo(teamGuid.Count());
+        int totalTeams = teamGuidList.Count();
+        int nextPowerOfTwo = Helpers.GetNextPowerOfTwo(totalTeams);
+        int previousPowerOfTwo = nextPowerOfTwo / 2;
         
-        int playingTeams = nextPowerOfTwo - totalTeams;
-        int byes = nextPowerOfTwo - playingTeams;
-        
-        var random = new Random();
-        int[] teamIndexesRandomized = Enumerable.Range(1, playingTeams).ToArray();
-        random.Shuffle(teamIndexesRandomized);
+        int amountOfByes = nextPowerOfTwo - totalTeams;
+        int amountOfPlayingTeams = totalTeams - amountOfByes;
+        int amountOfMatches = amountOfPlayingTeams * 2;
         
         
-        ///TODO: Check for byes and create list of MatchNode. Byes are Matchnodes with only PlayerA
+        List<int> randomizer = Enumerable.Range(1, totalTeams).ToArray().ToList();
+        var playingTeamsRandomized = randomizer.Take(new Range(0, amountOfPlayingTeams - 1)).ToArray();
+        var byesRandomized = randomizer.Take(new Range(amountOfPlayingTeams, randomizer.Count - 1)).ToArray();
+        
+        new Random().Shuffle(playingTeamsRandomized);
+        new Random().Shuffle(byesRandomized);
+        
+        // Splitting the randomized teams indexes into chunks of 2, creating MatchNodes accordingly
+        List<MatchNode> matches = new List<MatchNode>();
+        IEnumerable<int[]> playingTeamsChunks = playingTeamsRandomized.Chunk(2);
+        IEnumerable<int[]> byesChunks = byesRandomized.Chunk(2);
+        
+        foreach (var chunk in playingTeamsChunks)
+        {
+            matches.Add(new MatchNode()
+            {
+                IsBye = false,
+                TeamA = teamGuidList[chunk[0]],
+                TeamB = teamGuidList[chunk[1]]
+            });
+        }
+
+        foreach (var chunk in byesChunks)
+        {
+            matches.Add(new MatchNode()
+            {
+                IsBye = true,
+                TeamA = teamGuidList[chunk[0]],
+                TeamB = teamGuidList[chunk[1]]
+            });
+        }
+        return matches;
+    }
+
+    IEnumerable<MatchNode> ITournamentFormat.CreateFirstRoundSeeded(Dictionary<Guid, uint> teamGuidsWithRank)
+    {
+        // TODO: Implement Seeding
+        return new List<MatchNode>();
+    }
+
+    IEnumerable<MatchNode> ITournamentFormat.CreateNextRound(IEnumerable<MatchNode> completedRound)
+    {
+        List<MatchNode> completeRoundList = completedRound.ToList();
+
+        if (completeRoundList.Any(w => w.Winner == null))
+        {
+            throw new ArgumentException("All completed rounds must have a winning team.");
+        }
+
+        List<MatchNode> matches = new List<MatchNode>();
+        var previousMatchPairings = completeRoundList.Chunk(2);
+        foreach (var matchPairs in previousMatchPairings)
+        {
+            matches.Add(new MatchNode()
+            {
+                IsBye = false,
+                TeamA = matchPairs[0].Winner,
+                TeamB = matchPairs[1].Winner,
+                Winner = null,
+                Loser = null
+            });
+        }
+        return matches;
     }
 }
